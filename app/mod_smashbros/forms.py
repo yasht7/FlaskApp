29ca9d3c6b8a29557a739a3375b8bfcfe0fa6679
yasht7 <yashaswitamta@gmai.com>
2018-05-31 13:16:22 -0700

    buttons fixed

--- app/mod_smashbros/forms.py
+++ app/mod_smashbros/forms.py
@@ -4,6 +4,6 @@ from wtforms import Form, BooleanField, StringField, DecimalField, validators
 
 class NewCharacterForm(Form):
     name = StringField('Character Name', [validators.Length(min=1, max=25)])
-    weight = DecimalField('Weight', [validators.NumberRange(min=0, max=10000)])
+    weight = DecimalField('Weight (lbs)', [validators.NumberRange(min=0, max=10000)])
     powers = StringField('Moves', [validators.Length(min=1, max=100)])
-    speed = DecimalField('Speed', [validators.NumberRange(min=0, max=100)])
+    speed = DecimalField('Speed (0-100)', [validators.NumberRange(min=0, max=100)])
